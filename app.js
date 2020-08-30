a1b6e5627a49ff110d5bc8fbe50addff2c79fdf9
SreemanthG <sreemanth2001@gmail.com>
2020-08-30 14:10:47 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+
+mongoose.connect("mongodb://localhost/ConventionPlannerDemo");
+
+app.get("/",function(req,res){
+    req.send("Hello");
+})
+
+var PORT = process.env.PORT||3000;
+app.listen(PORT, function(){
+    console.log("App Started listening at "+ PORT)
+})
